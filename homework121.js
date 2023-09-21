4f6033df981119f1a17831f65f876f6834c3c191
Nastya Ladynskaya <feverrr27@gmail.com>
2023-09-21 19:49:21 +0300

    homework121

---
+++ homework121.js
@@ -0,0 +1,9 @@
+const num1 = parseFloat(prompt("Введіть перше число:"));
+const num2 = parseFloat(prompt("Введіть друге число:"));
+const num3 = parseFloat(prompt("Введіть третє число:"));
+
+
+const averageNumber = (num1 + num2 + num3) / 3;
+
+
+alert("Середнє арифметичне: " + averageNumber);
