b8f6dabb4235689c49c64db2ad104c3c2d9556ec
Nastya Ladynskaya <feverrr27@gmail.com>
2023-09-23 00:37:44 +0300

    homework131

---
+++ homework131.js
@@ -0,0 +1,20 @@
+// 1
+
+let input1 = prompt("Введіть перший рядок:");
+let input2 = prompt("Введіть другий рядок:");
+let input3 = prompt("Введіть третій рядок:");
+
+let concatenatedString = `${input1} ${input2} ${input3}`;
+console.log(concatenatedString);
+
+//2
+
+// let number = 12345; 
+// let numberString = number.toString();
+// let splitNumbers = numberString.split('').join(' ');
+
+// console.log(splitNumbers);
+
+
+
+
