209ed75a3723efba0de413847b4569a96ce58ee2
Nastya Ladynskaya <feverrr27@gmail.com>
2023-09-28 11:54:58 +0300

    homework141

---
+++ homework141.js
@@ -0,0 +1,17 @@
+const tableContainer = document.querySelector("#table-container");
+
+const table = document.createElement("table");
+
+let count = 1;
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement("tr");
+  for (let j = 0; j < 10; j++) {
+    const cell = document.createElement("td");
+    cell.textContent = count;
+    row.appendChild(cell);
+    count++;
+  }
+  table.appendChild(row);
+}
+
+tableContainer.appendChild(table);
