81e5441624ae92e9550c432fb93602d5a0575aa7
Nastya Ladynskaya <feverrr27@gmail.com>
2023-10-16 21:58:47 +0300

    homework191

---
+++ homework191.js
@@ -0,0 +1,49 @@
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  static SIZE_SMALL = { price: 50, calories: 20 };
+  static SIZE_LARGE = { price: 100, calories: 40 };
+  static STUFFING_CHEESE = { price: 10, calories: 20 };
+  static STUFFING_SALAD = { price: 20, calories: 5 };
+  static STUFFING_POTATO = { price: 15, calories: 10 };
+  static TOPPING_SPICE = { price: 15, calories: 0 };
+  static TOPPING_MAYO = { price: 20, calories: 5 };
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  calculatePrice() {
+    let price = this.size.price + this.stuffing.price;
+    for (const topping of this.toppings) {
+      price += topping.price;
+    }
+    return price;
+  }
+
+  calculateCalories() {
+    let calories = this.size.calories + this.stuffing.calories;
+    for (const topping of this.toppings) {
+      calories += topping.calories;
+    }
+    return calories;
+  }
+}
+
+
+const hamburger = new Hamburger(
+  Hamburger.SIZE_SMALL,
+  Hamburger.STUFFING_CHEESE
+);
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+
+console.log("Calories: " + hamburger.calculateCalories());
+console.log("Price: " + hamburger.calculatePrice());
+
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+
+console.log("Price with sauce: " + hamburger.calculatePrice());
